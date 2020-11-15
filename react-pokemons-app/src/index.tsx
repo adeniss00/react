6b1cab2d0e96fdea9a123180823cfecd198c928a
adeniss00 <adeniss@live.fr>
2020-11-15 12:55:55 +0100

    index.tsx

--- react-pokemons-app/src/index.tsx
+++ react-pokemons-app/src/index.tsx
@@ -1,8 +1,7 @@
  import React from 'react';
- import ReactDOM, { render } from 'react-dom';
  import App from './App';
-
+ import ReactDOM from 'react-dom';
+ 
  ReactDOM.render(
      <App/>,document.getElementById('root')
  );
- 
